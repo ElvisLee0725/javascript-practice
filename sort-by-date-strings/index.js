e9b4f752874977089f4d5a77b5844fd37e1ce337
ElvisLee <elvislee0725@gmail.com>
2021-07-04 17:40:07 -0700

    Made it object sort

--- sort-by-date-strings/index.js
+++ sort-by-date-strings/index.js
@@ -1,9 +1,32 @@
 // Use Date.parse() to parse a formatted date string to milliseconds 1/1/1970
 
-const arr = ["2019-11-21", "2019-11-22", "2010-12-31", "2020-01-24", "2019-11-23", "2019-10-21", "2019-01-21"];
+const arr = [
+  {
+    title: "What's SAP",
+    date: "2019-11-21"
+  }, {
+    title: "Artificial Mountains",
+    date: "2019-11-22"
+  }, {
+    title: "Simple text editor has 15k monthly users",
+    date: "2010-12-31"
+  }, {
+    title: "A message to our customers",
+    date: "2020-01-24"
+  }, {
+    title: "Alphabet earnings",
+    date: "2019-11-23"
+  }, {
+    title: "The Emu War",
+    date: "2019-10-21"
+  }, {
+    title: "Scaling to 100k Users",
+    date: "2019-01-21"
+  }
+];
 
-arr.sort((d1, d2) => {
-  return Date.parse(d1) < Date.parse(d2) ? 1 : -1;
+arr.sort((item1, item2) => {
+  return Date.parse(item1.date) < Date.parse(item2.date) ? 1 : -1;
 });
 
 console.log(arr);
